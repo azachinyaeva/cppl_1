4386a989177a096faad2d96a0895f9cb8992f1ca
Anna <annahasagun@yahoo.com>
2023-05-05 19:41:29 +0300

    first commit

---
+++ hw_1/hw_1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void print(std::vector<int> vec) {
+	for (auto num : vec) {
+		std::cout << num << " ";
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	std::vector<int> vec{ 4, 7, 9, 14, 12 };
+	std::cout << "Входные данные: ";
+	print(vec);
+	std::for_each(vec.begin(), vec.end(),
+		[&](int& num)
+		{
+			if ((num % 2) != 0) {
+				num *= 3;
+			}
+		});
+	std::cout << std::endl << "Выходные данные: ";
+	print(vec);
+}
---
+++ hw_2/hw_2.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <vector>
+#include <variant>
+
+std::variant <int, std::string, std::vector<int>> get_variant() {
+	std::srand(std::time(nullptr));
+	int random_variable = std::rand() % 3;
+
+	std::variant<int, std::string, std::vector<int>> result;
+	switch (random_variable)
+	{
+	case 0:
+		result = 5;
+		break;
+	case 1:
+		result = "string";
+		break;
+	case 2:
+		result = std::vector<int>{ 1, 2, 3, 4, 5 };
+		break;
+	default:
+		break;
+	}
+	return result;
+}
+
+int main()
+{
+	std::variant<int, std::string, std::vector<int>> result = get_variant();
+	if (std::holds_alternative<int>(result))
+	{
+		std::cout << std::get<int>(result) * 2 << std::endl;
+	}
+	else if (std::holds_alternative<std::string>(result))
+	{
+		std::cout << std::get<std::string>(result) << std::endl;
+	}
+	else if (std::holds_alternative<std::vector<int>>(result))
+	{
+		std::vector<int> vec = std::get<std::vector<int>>(result);
+		for (int i : vec)
+		{
+			std::cout << i << '\t';
+		}
+	}
+}
